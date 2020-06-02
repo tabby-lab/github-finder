7f1bf0b7c4cd34d319f73f8e08a5f13baf11ce0b
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-02 18:26:18 -0500

    Created my navbar component with a title and icon. Created useritem component with cards name and link to githubs. Brought in those components into app js.

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    state={
+        id:'id',
+        login:'mojo',
+        avatar_url:"https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url:"http://github.com/mojombo"
+    }
+        
+    
+    render() {
+        const{ login, avatar_url, html_url }=this.state;
+
+        return <div className="card text-center">
+            <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+    <h3> {login}</h3>
+    <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">more</a>
+    </div>
+        </div>;
+        
+    }
+}
+
+export default UserItem

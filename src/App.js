226e2325a1a8e4740856d93cec7bef1792fb2c10
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-03 19:02:28 -0500

    connected my env client and secret to my axios request

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ state= {
 
 //componentDidMount is a lifecycle method...render is also a lifecycle method
 async componentDidMount() {
+  console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
   this.setState({ loading: true });
 
-  const res = await axios.get('https://api.github.com/users');
+  const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
  
   this.setState({ users: res.data, loading: false });
 }

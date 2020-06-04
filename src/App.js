f641b943d78195a780c3cb5c74c18f119b28df08
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-03 21:20:23 -0500

    setstate for loading to true when searching for users.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ state= {
 
 //search github users
 searchUsers = async text => {
+  this.setState({ loading: true });
   const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
  
   this.setState({ users: res.data.items, loading: false });

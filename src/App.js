ac58ac7a7c122084404baf0fed75bf767d7db38b
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-03 19:12:17 -0500

    added search component with input text and submit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search'
 
 class App extends Component {
 state= {
@@ -25,6 +26,7 @@ async componentDidMount() {
       <div className='App'>
    <Navbar />
    <div className='container'>
+     <Search />
    <Users loading={this.state.loading} users={this.state.users} />
       </div>
       </div>

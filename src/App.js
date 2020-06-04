9080094b95af43a581e405c6309b047710a6a1be
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-04 17:00:09 -0500

    made a btn to clear the users.not working yet

--- src/App.js
+++ src/App.js
@@ -27,12 +27,16 @@ searchUsers = async text => {
   this.setState({ users: res.data.items, loading: false });
 }
 
+//clear users from state
+clearUsers= () => this.setState({ users:[], loading:false});
+
+
   render() {
     return (
       <div className='App'>
    <Navbar />
    <div className='container'>
-     <Search searchUsers={ this.searchUsers }/>
+     <Search searchUsers={ this.searchUsers } clearUsers={this.clearUsers}/>
    <Users loading={this.state.loading} users={this.state.users} />
       </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,8 @@ import PropTypes from 'prop-types';
      };
 
      static propTypes={
-         searchUsers: PropTypes.func.isRequired
+         searchUsers: PropTypes.func.isRequired,
+         clearUsers:PropTypes.func.isRequired 
      };
 
      onChange = (e) => 
@@ -35,6 +36,8 @@ this.setState({ [e.target.name]: e.target.value });
                     <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                 
                 </form>
+
+                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
 
              
             </div>

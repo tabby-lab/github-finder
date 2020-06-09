46e11a1314f2b52212bb9d3c78bdfba840ca9a7e
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-08 21:09:18 -0500

    Made about page and brought it into app js using route

--- src/App.js
+++ src/App.js
@@ -1,82 +1,84 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import Search from './components/users/Search';
-import Alert from './components/layouts/Alert';
-import { BrowserRouter as Router, Switch,Route } from 'react-router-dom'
-
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from './components/pages/About';
 
 class App extends Component {
-state= {
-  users: [],
-  loading: false,
-  alert: null
-}
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
 
-//componentDidMount is a lifecycle method...render is also a lifecycle method
-// async componentDidMount() {
-//   console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
-//   this.setState({ loading: true });
+  //componentDidMount is a lifecycle method...render is also a lifecycle method
+  // async componentDidMount() {
+  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+  //   this.setState({ loading: true });
 
-  
-// }
+  // }
 
-//search github users
-//all these methods deal  w state
-searchUsers = async text => {
-  this.setState({ loading: true });
-  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
- 
-  this.setState({ users: res.data.items, loading: false });
-}
+  //search github users
+  //all these methods deal  w state
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-//clear users from state
-clearUsers= () => this.setState({ users:[], loading:false});
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-//set Alert
-setAlert = (msg, type) => {
-  this.setState({alert:{msg ,type} });
+  //clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
-  setTimeout(() => this.setState({ alert:null }), 5000)
-};
+  //set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
 
-
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
-const { users, loading } = this.state;
+    const { users, loading } = this.state;
 
     return (
       <Router>
-      <div className='App'>
-   <Navbar />
-   <div className='container'>
-     <Alert alert={this.state.alert}/>
-     <Switch>
-       <Route exact path= '/' render={props=>(
-         <Fragment>
-<Search
-     searchUsers={ this.searchUsers } 
-     clearUsers={this.clearUsers} 
-     showClear={users.length > 0 ? true : false}
-     setAlert={this.setAlert}
-     />
-   <Users loading={loading} users={users} />
-         </Fragment>
-       )} />
-       </Switch>
-     
-      </div>
-      </div>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </div>
       </Router>
-      
     );
   }
 }
- 
 
 export default App;
 
 //showclear is an if else statement
 //destructoring bc we call this.state a few times/want to male clean
+//wrap all routes in a switch bc so that it shows one at a time

5bd1e82586bddd484782689d0845ac10609d91ab
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-12 12:02:17 -0500

    added bio,location,picture,github link to user

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ class App extends Component {
   searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data.items, loading: false });
@@ -37,10 +39,12 @@ class App extends Component {
 
   //get single github user
   getUser = async (username) => {
-    this.setState({ loading:false });
+    this.setState({ loading:true });
 
     const res = await axios.get(
-      `https://api.github.com/user?q=${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ user: res.data, loading: false });
     console.log(res.data)
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -39,7 +39,41 @@ static propTypes= {
         Back to search
       </Link>
       Hireable: {' '}
-{hireable ? <div className="badge badge-success">Hireable</div> : <div className="badge badge-danger">Not hireable</div>}
+{hireable ? (
+  <i className="fas fa-check success" /> 
+) : (
+<i className="fas fa-times-circle text-danger" />
+)}
+<div className="card grid-2">
+  <div className="all-center">
+    <img
+     src={avatar_url}
+     className="round-img"
+     alt='' 
+     style={{ width: '150px' }}
+     />
+     <h1>{name}</h1>
+     <p>Location: {location}</p>
+  </div>
+  <div>
+    {bio &&  ( 
+    <Fragment>
+      <h3>Bio</h3>
+      <p>{bio}</p>
+    </Fragment>
+    )}
+    <a href={html_url} className="btn btn-dark my-1">
+      Visit Github Profile
+    </a>
+    <ul>
+      <li>
+        {login && <Fragment>
+          <strong>Username:</strong>
+        </Fragment>}
+      </li>
+    </ul>
+  </div>
+</div>
     </Fragment>);
   }
 }
@@ -47,9 +81,3 @@ static propTypes= {
 export default User;
 
 //rce
-//Hireable: {' '}
-//{hireable ? (
-  //<i className="fas fa-check success" /> 
-//) : (
-//<i className="fas fa-times-circle text-danger" />
-//)}

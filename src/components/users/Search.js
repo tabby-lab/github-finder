7af24705416077740a7bba75f19451a8bf542764
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-15 14:19:59 -0500

    converted a class state component in search.js to a function state component using the useState hook.cleaning up code

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,61 +1,66 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
- class Search extends Component {
-     state = {
-         text: ''
-     };
-
-     static propTypes={
-         searchUsers: PropTypes.func.isRequired,
-         clearUsers:PropTypes.func.isRequired,
-         showClear:PropTypes.bool.isRequired,
-         setAlert:PropTypes.func.isRequired,
-     };
+//Bring in useState hook and define it using const and destructor
+//[create state, method of the state(usually "set" rhen name of state)]
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+   const [text,setText] = useState('');
 
 
-     onChange = (e) => 
-this.setState({ [e.target.name]: e.target.value });
-     
-     onSubmit = (e) => {
+
+
+    const onChange = e => setText(e.target.value);
+
+    const onSubmit = e => {
         e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('please enter something','light');
+        if (text === '') {
+            setAlert('please enter something', 'light');
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({ text: '' });
+            searchUsers(text);
+            setText('');
         }
-       
-     };
+
+    };
 
 
-    render(){
-        const { showClear, clearUsers } = this.props;
 
-        return (
-            <div>
-                <form  onSubmit={this.onSubmit} className="form">
-                 
-                    <input
-                     type="text"
-                     name="text"
-                     placeholder="Search Users..." 
-                     value={this.state.text}
-                     onChange={this.onChange} />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
-                
-                </form>
-                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
-                
 
-             
-            </div>
-        )
-    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+
+                <input
+                    type="text"
+                    name="text"
+                    placeholder="Search Users..."
+                    value={text}
+                    onChange={onChange} />
+                <input type="submit" value="Search" className="btn btn-dark btn-block" />
+
+            </form>
+            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+
+
+
+        </div>
+    )
+
 }
+Search.propTypes = {
+  
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
+    
+};
 
 export default Search
 
 //when there is a form in react usually you need to attach state to the input
 //destructoring showuser and clearuser
 //add method set alert
+
+//when adding a func in a func, use const
+//clean up: get rid of this.states
